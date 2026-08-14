579466fbbd32d890bb4cbd029ff21f4a0e2c79bf
ayomikun286 <edegbaiayomikun@gmail.com>
2026-08-14 16:32:07 +0100

    fix alert card

--- src/components/Alert-card.jsx
+++ src/components/Alert-card.jsx
@@ -5,7 +5,7 @@ const AlertCard = ({ errorMessage, showError, showSuccess }) => {
 
     return (
         <div
-            className={`fixed top-6 z-50 min-w-100 bg-[#030B1E] p-3 rounded border font-bold tracking-wider text-sm transition-all duration-300 ease-in-out
+            className={`fixed top-6 z-50  min-w-80 md:min-w-100 bg-[#030B1E] p-3 rounded border font-bold tracking-wider text-sm transition-all duration-300 ease-in-out
             ${showAlert ? 'right-0' : '-right-100'}
             ${showError
                     ? 'border-red-400 text-red-400'
